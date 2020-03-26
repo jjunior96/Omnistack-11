d08e61a3e1048589b31e71adc46f9b245ba52af7
Junior Alves <jjalves96@gmail.com>
2020-03-26 19:18:03 -0400

    feat: Incidents Controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
 const routes = express.Router();
 
 routes.post('/ongs', OngController.create);
 routes.get('/ongs', OngController.index);
+
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
 
 module.exports = routes;
