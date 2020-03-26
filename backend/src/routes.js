6da322b502b47db0002c38fd910e6d68639bcd4b
Junior Alves <jjalves96@gmail.com>
2020-03-26 19:33:06 -0400

    feat: route Profile

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,14 +2,19 @@ const express = require('express');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 
 const routes = express.Router();
 
 routes.post('/ongs', OngController.create);
 routes.get('/ongs', OngController.index);
 
+routes.get('/profile', ProfileController.index);
+
 routes.post('/incidents', IncidentController.create);
 routes.get('/incidents', IncidentController.index);
 routes.delete('/incidents/:id', IncidentController.delete);
+
+
 
 module.exports = routes;
