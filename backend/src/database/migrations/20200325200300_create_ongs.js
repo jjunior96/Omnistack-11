07dcc0fd4378cb7e1d703bd2cec482ac8a937394
Junior Alves <jjalves96@gmail.com>
2020-03-26 19:03:17 -0400

    feat: list & create ONGS' routes

--- backend/src/database/migrations/20200325200300_create_ongs.js
+++ backend/src/database/migrations/20200325200300_create_ongs.js
@@ -5,6 +5,7 @@ exports.up = function(knex) {
     table.string('name').notNullable();
     table.string('email').notNullable();
     table.string('whatsapp').notNullable();
+    table.string('city').notNullable();
     table.string('uf', 2).notNullable();
   })  
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,31 @@
 const express = require('express');
+const crypto = require('crypto');
+
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.post('/users', (req, res) => {
-  const body = req.body;
+routes.get('/ongs', async (req, res) => {
+  const ongs = await connection('ongs').select('*');
 
-  console.log(body);
+  return res.json(ongs);
+});
+
+routes.post('/ongs', async (req, res) => {
+  const { name, email, whatsapp, city, uf } = req.body;
+
+  const id = crypto.randomBytes(4).toString('HEX');
+
+  await connection('ongs').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf,
+  })
   
-  return res.json({
-    aluno: 'Junior'
-  });
+  return res.json({ id });
 });
 
 module.exports = routes;
